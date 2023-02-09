c178b1cafa74f31fe8240ea914127dee4250efa3
jean-claude-segura <candidatspam001@laposte.net>
2023-02-09 20:26:44 +0100

    Initial commit

---
+++ Google chat reader/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Google_chat_reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Discussion = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Chat
+            // 
+            this.Discussion.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Discussion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Discussion.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Discussion.Location = new System.Drawing.Point(12, 12);
+            this.Discussion.Name = "Chat";
+            this.Discussion.ReadOnly = true;
+            this.Discussion.Size = new System.Drawing.Size(776, 426);
+            this.Discussion.TabIndex = 0;
+            this.Discussion.Text = "Glisser-déposer le fichier messages.json";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Discussion);
+            this.Name = "Form1";
+            this.Text = "Google chat reader";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Discussion;
+    }
+}
+
---
+++ Google chat reader/Form1.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Google_chat_reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] droppedFilePath = (string[])e.Data.GetData(DataFormats.FileDrop);
+                var Content = JsonConvert.DeserializeObject<jsonFormat>(File.ReadAllText(droppedFilePath.FirstOrDefault()));
+                Discussion.SuspendLayout();
+                Discussion.Enabled = false;
+                if (Content.messages.Any())
+                {
+                    Discussion.Clear();
+                    string chatFile;
+                    var firstname = Content.messages.FirstOrDefault().creator.name;
+                    var previousname = String.Empty;
+                    var previousfulldate = String.Empty;
+                    var previousdate = String.Empty;
+                    foreach (var item in Content.messages)
+                    {
+                        var currentname = item.creator.name;
+                        var writename = currentname != previousname;
+                        previousname = currentname;
+
+                        var currentfulldate = item.created_date.Substring(
+                        0,
+                        item.created_date.Length - 7);
+                        var writefulldate = currentfulldate != previousfulldate;
+                        previousfulldate = currentfulldate;
+
+                        var currentdate = item.created_date.Substring(
+                        0,
+                        item.created_date.Length - 15);
+                        var writedate = currentdate != previousdate;
+                        previousdate = currentdate;
+
+                        Discussion.BackColor = Color.White;
+                        if(writedate)
+                        {
+                            Discussion.SelectionColor = Color.Black;
+                            Discussion.SelectionAlignment = HorizontalAlignment.Center;
+                            Discussion.AppendText(previousdate + Environment.NewLine);                            
+                        }
+                        //Chat.SelectionBackColor = currentname != firstname ? Color.Pink : Color.LightBlue;
+                        //Chat.SelectionBackColor = Color.White;
+                        //var test = Chat.Find("");
+                        Discussion.SelectionColor = currentname != firstname ? Color.DarkViolet : Color.Blue;
+                        Discussion.SelectionAlignment = HorizontalAlignment.Left; 
+                        string text = String.Empty;
+                        if (writefulldate || writename)
+                            text+= Environment.NewLine + item.creator.name + " " + currentfulldate + Environment.NewLine;
+                        /*else if (writedate)
+                            text += " " + item.created_date + Environment.NewLine;*/
+                        Discussion.AppendText(
+                            text +
+                            item.text + Environment.NewLine);
+                    }
+                }
+                Discussion.Enabled = true;
+                Discussion.ResumeLayout();
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+    }
+
+    public class jsonCreator
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string user_type { get; set; }
+    }
+    public class jsonMessage
+    {
+        public jsonCreator creator { get; set; }
+        public string created_date { get; set; }
+        public string text { get; set; }
+        public string topic_id { get; set; }
+
+        public override string ToString()
+        {
+            return text;
+        }
+    }
+    public class jsonFormat
+    {
+        public List<jsonMessage> messages { get; set; }
+    }
+}
